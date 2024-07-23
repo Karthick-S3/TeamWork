51f187a9d11a08fcd721896cb2e328678a3c653a
The-Viperpilot <karthickselva.sk@gmail.com>
2024-07-23 12:27:21 +0530

    moving cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+ 
+
 })
-export class HomeComponent  {
+export class HomeComponent implements OnInit  {
+  constructor(private ngxService : NgxUiLoaderService){}
+
+  cards = [1, 2, 3, 4, 5, 6,7,8,9,10]; 
+
+  ngOnInit(): void {
+   this.ngxService.start();
+   setTimeout(() => {
+    this.ngxService.stop();
+   }, 1000);
+  }
 
 }
